5764a3c14226da565e70124f79c6e7ba6c3c05be
Karan Gathani <karan.gathani@posit.co>
2024-11-14 12:35:47 -0800

    Add tests to run for every PR

--- tests/run_smoke_test.py
+++ tests/run_smoke_test.py
@@ -1,6 +1,5 @@
-import time
-
 from playwright.sync_api import Playwright, sync_playwright
+from utils import send_message_and_verify
 
 
 def run(playwright: Playwright):
@@ -8,21 +7,12 @@ def run(playwright: Playwright):
     browser = chromium.launch()
     page = browser.new_page()
     page.goto("https://gallery.shinyapps.io/assistant")
-    page.get_by_label("Python").check()
-    page.get_by_text("Who can see my activity?").click()
-    page.get_by_text("Who can see my activity?").click()
-    page.get_by_role("textbox", name="Enter a message...").click()
-    page.get_by_role("textbox", name="Enter a message...").fill(
-        """
-Help me write a shiny app with orange cards
-"""
+
+    send_message_and_verify(
+        page,
+        "Help me write a shiny app with orange cards",
+        "orange"
     )
-    page.get_by_label("Send message").click()
-    time.sleep(12)  # required since we are streaming responses
-    message_contents = page.query_selector_all(".message-content")
-    last_message_content = message_contents[-1].text_content()
-    if "orange" not in last_message_content:
-        raise AssertionError("Expected orange in last message content")
     browser.close()
 
 
